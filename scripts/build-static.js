a94e543630a200ee9ff815828421245401a1c2df
smgk <ganesh.krishna@gmail.com>
2026-07-26 20:50:37 -0700

    add static hosting build

---
+++ scripts/build-static.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const root = path.join(__dirname, "..");
+const output = path.join(root, "dist");
+const assets = [
+    "index.html",
+    "styles.css",
+    "app.js",
+    "meter_analysis.js",
+    "mishra.json",
+    "manifest.webmanifest",
+    "service-worker.js",
+    "icon.svg",
+    "privacy.html",
+    "terms.html",
+    "notices.html"
+];
+
+fs.rmSync(output, { recursive: true, force: true });
+fs.mkdirSync(output, { recursive: true });
+
+for (const asset of assets) {
+    fs.copyFileSync(path.join(root, asset), path.join(output, asset));
+}
+
+console.log(`Built ${assets.length} static assets in ${path.relative(root, output)}/.`);
